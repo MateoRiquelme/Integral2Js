9fc78ddd21a9baca8946d1c7020df133b1522186
MateoRiquelme <kuramaw12@gmail.com>
2023-07-06 00:40:52 -0300

    IntegralJs

--- js/index.js
+++ js/index.js
@@ -1,100 +1,171 @@
 import { productos } from "./productos.js";
 
-const cargaProductos=document.getElementById("cartas-container")
-const cardsTemplate=document.getElementById("paintCard").content
-const fragment=document.createDocumentFragment()
-const sideBar=document.getElementById("sideBar")
-const btnCarritoCompras=document.getElementById("btnCarritoCompras")
-const badge=document.getElementById("badge")
-const cart=document.getElementById("cart")
-const productoDentroCarrito=document.getElementById("productoDentroCarrito")
+const cargaProductos = document.getElementById("cartas-container");
+const cardsTemplate = document.getElementById("paintCard").content;
+const fragment = document.createDocumentFragment();
+const sideBar = document.getElementById("sideBar");
+const btnCarritoCompras = document.getElementById("btnCarritoCompras");
+const badge = document.getElementById("badge");
+const cart = document.getElementById("sideBar");
+const filtroItem = document.querySelectorAll(".filtroItem");
+const iconMenu = document.querySelector(".icon-menu")
+const menuHamburguesa = document.querySelector(".menu-hamburguesa")
 
-const carrito=[]
+// const musibara= new Audio("../musibara.mp3")
+// window.addEventListener("scroll", ()=>{
+//     if (window.scrollY>=0) {
+//      musibara.play()
+//      musibara.autoplay=true
+//      musibara.volume=0.05
+//      musibara.loop=true
+//     }
+// })
 
-document.addEventListener("DOMContentLoaded",()=>{
-    mostrarProductos(productos)
-})
+const productoDentroCarrito = document.getElementById(
+  "productoDentroCarrito"
+).content;
 
-const mostrarProductos=(productos)=> {
-    cargaProductos.innerHTML=""
+let carrito = {};
 
-    productos.map((item)=>{
-        const clon=cardsTemplate.cloneNode(true)
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarProductos(productos);
 
-    clon.getElementById("templateImg").setAttribute("src",item.imagen)
-    clon.getElementById("templateProducto").textContent=item.nombre
-    clon.getElementById("templatePrecio").textContent=item.precio
-    clon.getElementById("templateButton").dataset.id=item.id
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    mostrarCarrito();
+  }
+  menuHamburguesa.style.display="none"
+});
 
-fragment.appendChild(clon)
+const mostrarProductos = (productos) => {
+  cargaProductos.innerHTML = "";
 
+  productos.map((item) => {
+    const clon = cardsTemplate.cloneNode(true);
+
+    clon.getElementById("templateImg").setAttribute("src", item.imagen);
+    clon.getElementById("templateProducto").textContent = item.nombre;
+    clon.getElementById("templatePrecio").textContent = "$" + item.precio;
+    clon.getElementById("templateButton").dataset.id = item.id;
+
+    fragment.appendChild(clon);
+  });
+  cargaProductos.appendChild(fragment);
+};
+
+const agregarCarrito = (event) => {
+  if (event.target.classList.contains("btn")) {
+    guardarProducto(event.target.parentElement);
+  }
+  event.stopPropagation();
+};
+
+const guardarProducto = (producto) => {
+  const product = {
+    id: producto.querySelector("#templateButton").dataset.id,
+    imagen: producto.querySelector("#templateImg").getAttribute("src"),
+    nombre: producto.querySelector("#templateProducto").textContent,
+    precio: producto.querySelector("#templatePrecio").textContent,
+    cantidad: 1,
+  };
+
+  if (carrito.hasOwnProperty(product.id)) {
+    product.cantidad = carrito[product.id].cantidad + 1;
+  }
+
+  carrito[product.id] = { ...product };
+
+  mostrarCarrito();
+};
+
+cargaProductos.addEventListener("click", (event) => {
+  agregarCarrito(event);
+});
+
+btnCarritoCompras.addEventListener("click", () => {
+  if (sideBar.style.display === "none") {
+    sideBar.style.display = "block";
+  } else {
+    sideBar.style.display = "none";
+  }
+});
+
+const pintarBadge = () => {
+  const nCantidad = Object.values(carrito).reduce(
+    (acc, { cantidad }) => acc + cantidad,
+    0
+  );
+  if (nCantidad === 0) {
+    badge.style.display = "none";
+  } else {
+    badge.style.display = "block";
+    badge.textContent = nCantidad;
+  }
+};
+
+const mostrarCarrito = () => {
+  cart.innerHTML = "";
+  Object.values(carrito).forEach((productoSelecionado) => {
+    let str = productoSelecionado.precio;
+    let num = str.replace(/\$/g, "");
+
+    productoDentroCarrito
+      .querySelector(".templateAñadidoCarritoImg")
+      .setAttribute("src", productoSelecionado.imagen);
+    productoDentroCarrito.querySelector(
+      ".templateAñadidoCarritoProducto"
+    ).textContent = productoSelecionado.nombre;
+    productoDentroCarrito.querySelector(
+      ".templateCantidadCarrito"
+    ).textContent = productoSelecionado.cantidad;
+    productoDentroCarrito.querySelector(
+      ".templateAñadidoCarritoPrecio"
+    ).textContent = "$" + parseInt(num) * productoSelecionado.cantidad;
+    productoDentroCarrito.querySelector(".borrarCarrito").dataset.id =
+      productoSelecionado.id;
+
+    const clonTwo = productoDentroCarrito.cloneNode(true);
+    fragment.appendChild(clonTwo);
+  });
+  cart.appendChild(fragment);
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+
+  if (localStorage.getItem("carrito") === "{}") {
+    cart.innerHTML = `<p class="pSideBar">No hay carpinchos en el carritobara 😥</p>`;
+  }
+
+  pintarBadge();
+};
+
+sideBar.addEventListener("click", (e) => {
+  cartBtnActions(e);
+});
+
+const cartBtnActions = (e) => {
+  if (e.target.classList.contains("borrarCarrito")) {
+    delete carrito[e.target.dataset.id];
+    mostrarCarrito();
+  }
+  e.stopPropagation();
+};
+
+filtroItem.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        const itemFiltrado = item.textContent
+        if (itemFiltrado !== "todos") {
+            const capibaraYaFiltrado = productos.filter((capibara)=>capibara.rareza===itemFiltrado)
+            mostrarProductos(capibaraYaFiltrado)
+        }
+        else {
+            mostrarProductos(productos)
+        }
     })
-cargaProductos.appendChild(fragment)
-}
-
-const agregarCarrito=(event)=>{
-    if (event.target.classList.contains("btn")) {
-      guardarProducto(event.target.parentElement) 
-    }
-event.stopPropagation()
-}
-
-const guardarProducto=(producto)=>{
-    const product={
-        id: producto.querySelector("#templateButton").dataset.id,
-        imagen:producto.querySelector("#templateImg").getAttribute("src",),
-        nombre:producto.querySelector("#templateProducto").textContent,
-        precio:producto.querySelector("#templatePrecio").textContent,
-        cantidad:1
-    }
-
-const productosExistentes= carrito.findIndex((item)=>item.id===product.id)
-
-if (productosExistentes!==-1) {
-    carrito[productosExistentes].cantidad+=1
-} else{
-    carrito.push(product)
-}
-pintarBadge()
-mostrarCarrito()
-}
-
-cargaProductos.addEventListener("click",(event)=>{
-    agregarCarrito(event)
 })
 
-btnCarritoCompras.addEventListener("click",()=>{
-    if (sideBar.style.display==="none") {
-        sideBar.style.display="block"
-    }
-    else{
-        sideBar.style.display="none"
-    }
+iconMenu.addEventListener("click", ()=>{
+ if ( menuHamburguesa.style.display==="none") {
+  menuHamburguesa.style.display="block"
+ }
+ else { menuHamburguesa.style.display="none"}
 })
-
-const pintarBadge=()=>{
-const nCantidad=carrito.reduce((acc,{cantidad})=>acc+cantidad,0)
-if (nCantidad===0) {
-    badge.style.display="none"
-}
-else{
-    badge.style.display="block"
-    badge.textContent=nCantidad
-}
-}
-
-const mostrarCarrito=()=>{
-    cart.innerHTML=""
-    carrito.forEach((productoSelecionado)=>{
-        productoDentroCarrito.querySelector(".productoDentroCarrito").setAttribute("src",productoSelecionado.imagen)
-        productoDentroCarrito.querySelector(".productoDentroCarrito").textContent = productoSelecionado.nombre
-        productoDentroCarrito.querySelector(".productoDentroCarrito").textContent = productoSelecionado.precio
-
-        const clonTwo=productoDentroCarrito.cloneNode(true)
-        fragment.appendChild(clonTwo)
-    })
-cart.appendChild(fragment)
-pintarBadge()
-}
-
-
--- js/productos.js
+++ js/productos.js
@@ -1,178 +1,258 @@
 const productos=[
     {
         id:1,
-        nombre:"c1",
-        precio:10,
+        nombre:"Guerrero Capibara",
+        precio:103,
         imagen:"../cards/guerreroCapibara.webp",
+        rareza:"comun"
     },
     {
         id:2,
-        nombre:"c2",
-        precio:15,
+        nombre:"Carpincho Uruguayo",
+        precio:600,
         imagen:"../cards/CarpinchoUruguayo.jfif",
+        rareza:"comun"
     },
     {
         id:3,
-        nombre:"c3",
-        precio:12,
+        nombre:"Pack de capibaras tacticos",
+        precio:1200,
         imagen:"../cards/capibara tactico.webp",
+        rareza:"epico"
     },
     {
         id:4,
-        nombre:"c4",
-        precio:1,
+        nombre:"Rambobara",
+        precio:34,
         imagen:"../cards/ramboCapibara.jpg",
+        rareza:"poco comun"
     },
     {
         id:5,
-        nombre:"c5",
+        nombre:"Budincho",
         precio:100,
         imagen:"../cards/Budincho.jfif",
+        rareza:"poco comun"
     },
     {
         id:6,
-        nombre:"c6",
+        nombre:"Carpinchonnut",
         precio:18,
         imagen:"../cards/capibaradonnut.jfif",
+        rareza:"raro"
     },
     {
         id:7,
-        nombre:"c7",
+        nombre:"Carpincho Error delsistema",
         precio:20,
         imagen:"../cards/capibaraerrorDeSistema.jfif",
+        rareza:"comun"
     },
     {
         id:8,
-        nombre:"c8",
+        nombre:"Diablibara",
         precio:130,
         imagen:"../cards/capibaraLucy.jfif",
+        rareza:"raro"
+
     },
     {
         id:9,
-        nombre:"c9",
+        nombre:"Carpincho Chamuyero",
         precio:1230,
         imagen:"../cards/capibaraponedor.jfif",
+        rareza:"poco comun"
+
     },
     {
         id:10,
-        nombre:"c10",
-        precio:10,
+        nombre:"carpichangel",
+        precio:100,
         imagen:"../cards/Carpinchangel.jfif",
+        rareza:"poco comun"
+
     },
     {
         id:11,
-        nombre:"c11",
-        precio:10,
+        nombre:"Capibara america",
+        precio:500,
         imagen:"../cards/capitanAmerica.jfif",
+        rareza:"legendario"
+
     },
     {
         id:12,
-        nombre:"c12",
-        precio:10,
+        nombre:"Baby capibara",
+        precio:5,
         imagen:"../cards/CarpinchoBb.jfif",
+        rareza:"comun",
+
     },
     {
         id:13,
-        nombre:"c13",
-        precio:10,
+        nombre:"Carpincho de boquita",
+        precio:74,
         imagen:"../cards/carpinchoBostero.jfif",
+        rareza:"epico",
+
     },
     {
         id:14,
-        nombre:"c14",
+        nombre:"Carpincho Rogue",
         precio:10,
         imagen:"../cards/carpinchoespadaroja.jfif",
+        rareza:"comun",
+
     },
     {
         id:15,
-        nombre:"c15",
-        precio:10,
+        nombre:"Gladibarador",
+        precio:19,
         imagen:"../cards/carpinchoGladiador.jfif",
+        rareza:"epico",
+
     },
     {
         id:16,
-        nombre:"c16",
-        precio:10,
+        nombre:"Super Capibara",
+        precio:1605,
         imagen:"../cards/carpinchoHero.png",
+        rareza:"legendario"
+
     },
     {
         id:17,
-        nombre:"c17",
-        precio:10,
+        nombre:"Carpincho Años 80",
+        precio:80,
         imagen:"../cards/carpinchoochenteero.jfif",
     },
     {
         id:18,
-        nombre:"c18",
-        precio:10,
+        nombre:"Carpincho Patrio",
+        precio:1230,
         imagen:"../cards/carpinchoPatrio.jfif",
+        rareza:"mitico"
+
     },
     {
         id:19,
-        nombre:"c19",
-        precio:10,
+        nombre:"Capiberto",
+        precio:2,
         imagen:"../cards/carpinchoPresidente.jpg",
+        rareza:"comun"
+
     },
     {
         id:20,
-        nombre:"c20",
-        precio:10,
+        nombre:"Carpincho riber",
+        precio:2611,
         imagen:"../cards/CarpinchoRiver.jfif",
+        rareza:"epico"
+
     },
     {
         id:21,
-        nombre:"c21",
-        precio:10,
+        nombre:"Carpincho Samurai",
+        precio:3,
         imagen:"../cards/carpinchoSAmurai.jfif",
+        rareza:"comun"
+
     },
     {
         id:22,
-        nombre:"c22",
-        precio:10,
+        nombre:"CArpincho dia de playa",
+        precio:16,
         imagen:"../cards/carpinchoSombrero.jfif",
+        rareza:"raro"
+
     },
     {
         id:23,
-        nombre:"c23",
-        precio:10,
+        nombre:"Carpincho Uruguayo",
+        precio:32,
         imagen:"../cards/CarpinchoUruguayo.jfif",
+        rareza:"comun"
+
     },
     {
         id:24,
         imagen:"../cards/cyberbara.jfif",
-        nombre:"c24",
-        precio:10,
+        nombre:"cyberbara",
+        precio:13,
+        rareza:"comun"
+
     },
     {
         id:25,
         imagen:"../cards/dulceCapibara.jfif",
-        nombre:"c25",
+        nombre:"Dulce Capibara",
         precio:10,
+        rareza:"comun"
+
     },
     {
         id:26,
         imagen:"../cards/gokuCarpincho.jpg",
-        nombre:"c26",
-        precio:10,
+        nombre:"kakarpincho",
+        precio:200,
+        rareza:"epico"
+
     },
     {
         id:27,
         imagen:"../cards/carpinnchofachero.jfif",
-        nombre:"c27",
-        precio:10,
+        nombre:"carpinncho fachero",
+        precio:100,
+        rareza:"raro"
+
     },
     {
         id:28,
         imagen:"../cards/carpinku.jpg",
-        nombre:"c28",
-        precio:10,
+        nombre:"Carpinku",
+        precio:170,
+        rareza:"epico"
+
     },
     {
         id:29,
         imagen:"../image/carpinchoMain.jfif",
-        nombre:"c28",
-        precio:10,
+        nombre:"Pack de pokebaras",
+        precio:1000,
+        rareza:"legendario"
+
+    },
+    {
+        id:30,
+        imagen:"../cards/Default_capybara_batman_super_hero_0_9cd11538-5d30-4202-867a-f2ac97cc8edf_1.jpg",
+        nombre:"Batbara",
+        precio:930,
+        rareza:"legendario"
+    },
+    {
+        id:30,
+        imagen:"../cards/Leonardo_Diffusion_capybara_vegito_dragon_ball_z_0.jpg",
+        nombre:"Vegibara",
+        precio:730,
+        rareza:"epico"
+    },
+    {
+        id:30,
+        imagen:"../cards/Leonardo_Diffusion_capybara_electrician_1.jpg",
+        nombre:"Capibara de refsa",
+        precio:730,
+        rareza:"epico"
+    },
+    {
+        id:30,
+        imagen:"../image/capibaraCs",
+        nombre:"Fusibara",
+        precio:730,
+        rareza:"epico"
     },
 ]
+
+
+
 
 export{productos}
